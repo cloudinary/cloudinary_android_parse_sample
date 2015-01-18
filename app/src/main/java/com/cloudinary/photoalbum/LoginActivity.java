a4bc94e28cece670af904e4cab8d140f5955d0c7
Daniel Cohen <daniel@codeinvain.com>
2015-01-18 12:48:56 +0200

    moved to android studio

---
+++ app/src/main/java/com/cloudinary/photoalbum/LoginActivity.java
@@ -0,0 +1,278 @@
+package com.cloudinary.photoalbum;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+/**
+ * Activity which displays a login screen to the user, offering registration as
+ * well.
+ */
+public class LoginActivity extends Activity {
+	/**
+	 * Keep track of the login task to ensure we can cancel it if requested.
+	 */
+	private UserLoginTask mAuthTask = null;
+
+	private Context mContext = this;
+	
+	// Values for user and password at the time of the login attempt.
+	private String mUser;
+	private String mPassword;
+
+	// UI references.
+	private EditText mUserView;
+	private EditText mPasswordView;
+	private View mLoginFormView;
+	private View mLoginStatusView;
+	private TextView mLoginStatusMessageView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_login);
+		setupActionBar();
+		
+		setupImage();
+
+		// Set up the login form.
+		mUserView = (EditText) findViewById(R.id.user);
+		mUserView.setText(mUser);
+
+		mPasswordView = (EditText) findViewById(R.id.password);
+		mPasswordView
+				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+					@Override
+					public boolean onEditorAction(TextView textView, int id,
+							KeyEvent keyEvent) {
+						if (id == R.id.login || id == EditorInfo.IME_NULL) {
+							attemptLogin();
+							return true;
+						}
+						return false;
+					}
+				});
+
+		mLoginFormView = findViewById(R.id.login_form);
+		mLoginStatusView = findViewById(R.id.login_status);
+		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
+
+		findViewById(R.id.sign_in_button).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						attemptLogin();
+					}
+				});
+	}
+	
+	void setupImage() {
+		Cloudinary cloudinary = PhotoAlbumApplication.getInstance(this).getCloudinary();
+		String url = cloudinary.url().type("facebook").transformation(
+			new Transformation().height(95).width(95).crop("thumb").gravity("face").effect("sepia").radius(20)
+			.chain().angle(10)
+		).format("png").generate("officialchucknorrispage");
+		new DownloadImageTask((ImageView) findViewById(R.id.logo))
+            .execute(url);
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			// Show the Up button in the action bar.
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * Attempts to sign in or register the account specified by the login form.
+	 * If there are form errors (invalid user, missing fields, etc.), the
+	 * errors are presented and no actual login attempt is made.
+	 */
+	public void attemptLogin() {
+		if (mAuthTask != null) {
+			return;
+		}
+
+		// Reset errors.
+		mUserView.setError(null);
+		mPasswordView.setError(null);
+
+		// Store values at the time of the login attempt.
+		mUser = mUserView.getText().toString();
+		mPassword = mPasswordView.getText().toString();
+
+		boolean cancel = false;
+		View focusView = null;
+
+		// Check for a valid password.
+		if (TextUtils.isEmpty(mPassword)) {
+			mPasswordView.setError(getString(R.string.error_field_required));
+			focusView = mPasswordView;
+			cancel = true;
+		} else if (mPassword.length() < 4) {
+			mPasswordView.setError(getString(R.string.error_invalid_password));
+			focusView = mPasswordView;
+			cancel = true;
+		}
+
+		// Check for a valid user
+		if (TextUtils.isEmpty(mUser)) {
+			mUserView.setError(getString(R.string.error_field_required));
+			focusView = mUserView;
+			cancel = true;
+		}
+
+		if (cancel) {
+			// There was an error; don't attempt login and focus the first
+			// form field with an error.
+			focusView.requestFocus();
+		} else {
+			// Show a progress spinner, and kick off a background task to
+			// perform the user login attempt.
+			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+			showProgress(true);
+			mAuthTask = new UserLoginTask();
+			mAuthTask.execute((Void) null);
+		}
+	}
+
+	/**
+	 * Shows the progress UI and hides the login form.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+	private void showProgress(final boolean show) {
+		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+		// for very easy animations. If available, use these APIs to fade-in
+		// the progress spinner.
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+			int shortAnimTime = getResources().getInteger(
+					android.R.integer.config_shortAnimTime);
+
+			mLoginStatusView.setVisibility(View.VISIBLE);
+			mLoginStatusView.animate().setDuration(shortAnimTime)
+					.alpha(show ? 1 : 0)
+					.setListener(new AnimatorListenerAdapter() {
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							mLoginStatusView.setVisibility(show ? View.VISIBLE
+									: View.GONE);
+						}
+					});
+
+			mLoginFormView.setVisibility(View.VISIBLE);
+			mLoginFormView.animate().setDuration(shortAnimTime)
+					.alpha(show ? 0 : 1)
+					.setListener(new AnimatorListenerAdapter() {
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							mLoginFormView.setVisibility(show ? View.GONE
+									: View.VISIBLE);
+						}
+					});
+		} else {
+			// The ViewPropertyAnimator APIs are not available, so simply show
+			// and hide the relevant UI components.
+			mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+		}
+	}
+
+	/**
+	 * Represents an asynchronous login/registration task used to authenticate
+	 * the user.
+	 */
+	public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+		@Override
+		protected Boolean doInBackground(Void... params) {
+			ParseUser user;
+			try {
+				// Parse: Look up given user
+				L.d("UserLoginTask::doInBackground - checking if user '%s' exists", mUser);
+				ParseQuery<ParseUser> query = ParseQuery.getQuery(ParseUser.class).whereEqualTo("username", mUser);
+				if (query.count() == 0) {
+					L.i("UserLoginTask::doInBackground - doesn't exist - signing up");
+					// Parse: User doesn't exist - create
+					user = new ParseUser();
+					user.setUsername(mUser);
+					user.setPassword(mPassword);
+					user.signUp();
+				} else {
+					L.d("UserLoginTask::doInBackground - exists - verifying password");
+					// Parse: User exists - verify password
+					ParseUser.logIn(mUser, mPassword);
+				}
+				L.d("UserLoginTask::doInBackground - done!");
+
+				return true;
+			} catch (ParseException e) {
+				L.e(e, "Authentication error");
+				return false;
+			}
+		}
+
+		@Override
+		protected void onPostExecute(final Boolean success) {
+			mAuthTask = null;
+			showProgress(false);
+
+			if (success) {
+				Intent intent = new Intent(mContext, ListPhotosActivity.class);
+				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(intent);
+			} else {
+				mPasswordView
+						.setError(getString(R.string.error_incorrect_password));
+				mPasswordView.requestFocus();
+			}
+		}
+
+		@Override
+		protected void onCancelled() {
+			mAuthTask = null;
+			showProgress(false);
+		}
+	}
+}
---
+++ app/src/main/java/com/cloudinary/photoalbum/UploadPhotoActivity.java
@@ -0,0 +1,182 @@
+package com.cloudinary.photoalbum;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.cloudinary.Cloudinary;
+import com.parse.ParseCloud;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+
+public class UploadPhotoActivity extends Activity {
+	private final static int SELECT_PICTURE = 1;
+	private final Activity current = this;
+	private ProgressDialog dialog = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Intent intent = new Intent();
+		intent.setType("image/*");
+		intent.setAction(Intent.ACTION_GET_CONTENT);
+		startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == SELECT_PICTURE) {
+			if (resultCode == RESULT_OK) {
+				setDefaultLayout();
+		        Uri selectedImageUri = data.getData();
+		        L.d("Uploading file from URI: %s", selectedImageUri.getPath());
+	            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+	
+	            Cursor cursor = getContentResolver().query(
+	                               selectedImageUri, filePathColumn, null, null, null);
+	            cursor.moveToFirst();
+	            
+				int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+	            String filePath = cursor.getString(columnIndex);
+	            cursor.close();
+		        L.d("Uploading file: %s", filePath);
+		        startUpload(filePath);
+			}
+		}
+	}
+	
+	private void setDefaultLayout() {
+		setContentView(R.layout.activity_upload_photo);
+		// Show the Up button in the action bar.
+		setupActionBar();
+	}
+	
+	private void startUpload(String filePath) {
+		AsyncTask<String, Void, String> task = new AsyncTask<String, Void, String>() {
+			protected String doInBackground(String... paths) {
+				L.d("Running upload task");
+
+				// sign request
+				Map<String, String> uploadParams;
+				try {
+					// Parse+Cloudinary: retrieves a Cloudinary signature and upload params using the Parse cloud function.
+					//   see https://github.com/cloudinary/cloudinary_parse
+                    HashMap<String, String> args = new HashMap<String, String>();
+                    uploadParams = ParseCloud.callFunction(Constants.PARSE_SIGN_CLOUD_FUNCTION ,args);
+			        L.i("Signed request: %s", uploadParams.toString());
+				} catch (ParseException e) {
+			        L.e(e, "Error signing request");
+					return "Error signing request: " + e.toString();
+				}
+		
+				// Upload to cloudinary
+				Cloudinary cloudinary = PhotoAlbumApplication.getInstance(current).getCloudinary();
+				File file = new File(paths[0]);
+				@SuppressWarnings("rawtypes")
+				Map cloudinaryResult;
+				try {
+					// Cloudinary: Upload file using the retrieved signature and upload params
+					cloudinaryResult = cloudinary.uploader().upload(file, uploadParams);
+			        L.i("Uploaded file: %s", cloudinaryResult.toString());
+				} catch (RuntimeException e) {
+			        L.e(e, "Error uploading file");
+					return "Error uploading file: " + e.toString();
+				} catch (IOException e) {
+			        L.e(e, "Error uploading file");
+					return "Error uploading file: " + e.toString();
+				}
+		
+				// update parse
+				ParseObject photo = new ParseObject("Photo");
+				try {
+					// Parse+Cloudinary: Save a reference to the uploaded image in Parse backend. The
+					//   field may be verified using the beforeSave filter demonstrated in:
+					//   https://github.com/cloudinary/cloudinary_parse
+					photo.put(Constants.PARSE_CLOUDINARY_FIELD, cloudinary.signedPreloadedImage(cloudinaryResult));
+					photo.save();
+			        L.i("Saved object");
+				} catch (Exception e) {
+			        L.e(e, "Error saving object");
+					return "Error saving object: " + e.toString();
+				}
+				return null;
+			}
+
+			protected void onPostExecute(String error) {
+				if (dialog != null) {
+					dialog.dismiss();
+					dialog = null;
+				}
+				if (error == null) {
+					setResult(RESULT_OK);
+					finish();
+				} else {
+					new AlertDialog.Builder(current)
+						.setTitle("Error")
+						.setMessage(error)
+						.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int whichButton) {
+								finish();
+							}
+						})
+						.setCancelable(true)
+						.create().show();
+				}
+			}
+		};
+		dialog = ProgressDialog.show(this, "Uploading", "Uploading image");
+		task.execute(filePath);
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.upload_photo, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
